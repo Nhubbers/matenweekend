dc36df1353b94b0d438a85666c23d288ce48096d
Nick Hubbers <nhubbers@gmail.com>
2026-06-10 17:40:38 +0200

    fix: change view options structure in rankings_view migration for PocketBase v0.23+

--- matenweekend_config/pocketbase_config/pb_migrations/1718023600_create_rankings_view.js
+++ matenweekend_config/pocketbase_config/pb_migrations/1718023600_create_rankings_view.js
@@ -1,21 +1,23 @@
-migrate((app) => {
-    const collection = new Collection({
-        id: "pbc_rankings_view",
-        name: "rankings_view",
-        type: "view",
-        listRule: "@request.auth.id != ''",
-        viewRule: "@request.auth.id != ''",
-        options: {
-            query: "SELECT u.id, u.name, u.avatar, COALESCE(SUM(pt.amount), 0) as totalPoints FROM users u LEFT JOIN point_transactions pt ON pt.user = u.id GROUP BY u.id"
-        }
-    });
+migrate(
+    (app) => {
+        const collection = new Collection({
+            id: 'pbc_rankings_view',
+            name: 'rankings_view',
+            type: 'view',
+            listRule: "@request.auth.id != ''",
+            viewRule: "@request.auth.id != ''",
+            viewQuery:
+                'SELECT u.id, u.name, u.avatar, COALESCE(SUM(pt.amount), 0) as totalPoints FROM users u LEFT JOIN point_transactions pt ON pt.user = u.id GROUP BY u.id',
+        });
 
-    return app.save(collection);
-}, (app) => {
-    try {
-        const collection = app.findCollectionByNameOrId("rankings_view");
-        return app.delete(collection);
-    } catch (err) {
-        return null;
+        return app.save(collection);
+    },
+    (app) => {
+        try {
+            const collection = app.findCollectionByNameOrId('rankings_view');
+            return app.delete(collection);
+        } catch (err) {
+            return null;
+        }
     }
-});
+);
